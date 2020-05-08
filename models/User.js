0ace36310f1e20bd7bc0fb0db83e652687488829
hshine1226 <hshine1226@gmail.com>
2020-05-08 16:33:44 +0900

    기능: 로그인 기능 with Bcrypt(1)
    요청된 이메일이 데이터베이스에 있는지 확인한다.
    bcrypt.compare를 이용해서 plainPassword를 암호화 해서 비교한다.

--- models/User.js
+++ models/User.js
@@ -52,8 +52,19 @@ userSchema.pre("save", function (next) {
         next();
       });
     });
+  } else {
+    next();
   }
 });
+
+userSchema.methods.comparePassword = function (plainPassword, cb) {
+  //plainPassword와 암호화된 비밀번호가 같은지 체크해야 함
+  //plainPassword를 암호화 해서 원래 암호화된 비밀번호와 같은지 체크해야함
+  //암호화된 password를 복호화 할 수는 없음
+  bcrypt.compare(plainPassword, this.password, function (err, isMatch) {
+    if (err) return cb(err), cb(null, isMatch);
+  });
+};
 
 const User = mongoose.model("User", userSchema);
 
