b074e0662edfd85d647e9d20681aecc912cba66a
hshine1226 <hshine1226@gmail.com>
2020-05-08 14:33:05 +0900

    Postman을 이용해서 회원가입 정보를 mongoDB에 넘김

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const app = express();
 const port = 5000;
+const bodyParser = require("body-parser");
+
+// application/x-www-form-urlencloded  형식으로 된 데이터를 분석해서 가져올 수 있게 해줌
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//application/json 으로 된 데이터를 분석해서 가져올 수 있게 해줌
+app.use(bodyParser.json());
+
+const { User } = require("./models/User");
 
 const mongoose = require("mongoose");
 mongoose
@@ -17,6 +26,19 @@ mongoose
   .catch((err) => console.log(err));
 
 app.get("/", (req, res) => res.send("Hello World!!!"));
+
+app.post("/register", (req, res) => {
+  // body-parser를 통해서 req.body로 client에서 보내주는 정보를 받아준다.
+  const user = new User(req.body);
+
+  // 몽고 db에서 오는 메소드이다.
+  user.save((err, userInfo) => {
+    if (err) return res.json({ success: false, err });
+    return res.status(200).json({
+      success: true,
+    });
+  });
+});
 
 app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
